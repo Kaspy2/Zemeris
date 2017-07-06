b848adf29184ebd4e1918f7cd4dfc4b08a771f0d
ASCENT\Gabrielf <gabriel.farrugia@ascent.software>
2017-07-06 17:00:27 +0200

    Moved paragraph segmentation method to ParaProc

--- Zemeris/ParaProc.cs
+++ Zemeris/ParaProc.cs
@@ -96,10 +96,163 @@ namespace Zemeris
             return (LineLeft(proper, b1) < LineLeft(proper, b2) + limit) && (LineLeft(proper, b1) > LineLeft(proper, b2) - limit);
         }
 
-        public int CaseChecker(List<string[]> proper, int b)    //title, subtitle, word, unknown
+        public List<string> GenerateParagraphs(List<string[]> proper, int limit)
         {
-            return 1;
+            List<string> paragraphs = new List<string>();
+            StringBuilder strcur = new StringBuilder();     //current par
+            int listMarg = 0, curPage = 1, margLineNum = 0;
+            bool dashDetect = false, flush = false, newParNextLine = false, dashDone = false, listDetect = false;
+            
+            //if last is ':' get next line's left, next next line's left (lookahead - make sure not same) to get indentation levels
+
+            //if same indentation levels add line
+
+            // ; or . add para
+            //if punc mark and next line not same right level
+
+            //if not same right level, flush to para
+            //if curr line less right level than next line flush to para ---------------
+
+            //keep checking deeper for same indentation until same found, once same found get the first line with the different indentation and generate a paragraph
+
+            for (int b = 0; b < proper.Count; b++)            //cycle through proper
+            {
+                if (Int32.Parse(proper[b][6]) == -1)         //if -1, check next line to see if same indentation
+                {
+                    //bool sameMarg = true;
+                    if (Int32.Parse(proper[b][5]) != curPage)   //seperates pages
+                    {
+                        curPage = Int32.Parse(proper[b][5]);
+                        paragraphs.Add(strcur.ToString());
+                        strcur = new StringBuilder();
+                        flush = true;
+                    }
+
+                    while (b < proper.Count && Int32.Parse(proper[b][6]) == -1)     //skip empties
+                    {
+                        b++;
+                    }
+
+                    if (dashDetect)
+                    {
+                        int rewind = b;
+                        dashDetect = false;
+                        while (b < proper.Count && Int32.Parse(proper[b][6]) != -1 && Int32.Parse(proper[b][5]) == curPage)     //add the whole line
+                        {
+                            strcur.Append(proper[b][0] + " ");
+                            b++;
+                        }
+                        dashDone = true;
+                        b = rewind;
+                        //now at last element in line after dash
+                    }
+
+                    if (!flush && !listDetect)
+                    {
+                        if (NLR(proper, b, limit) && NLL(proper, b, limit))    //if same right and left indentation
+                        {
+                            //          | |
+                            flush = false;  //since no line indentation change means that should be in same paragraph
+                        }
+                        else if ((LineRight(proper, b) - limit > NextLineRight(proper, b))  //if same left indentation but next line is shorter than this line
+                            && NLL(proper, b, limit))
+                        {
+                            //          | /
+                            newParNextLine = true;  //as next line is end of para
+                        }
+                        else if (NextLineRight(proper, b) - limit > LineRight(proper, b))  //if next line ends more to the right
+                        {   //if this line is shorter than next, also next para
+                            //this means end of current par so flush to para
+                            //          | \
+                            flush = true;
+                        }
+                        else if (NextLineLeft(proper, b) - limit > LineLeft(proper, b))     //if next line is indented
+                        {   //next line starts a new para
+                            //          \
+                            flush = true;
+                        }
+                    }
+
+                    if (!dashDone)    //if no dashes were processed
+                    {
+                        while (b < proper.Count && Int32.Parse(proper[b][6]) != -1)     //add the whole line to the current paragraph
+                        {
+                            strcur.Append(proper[b][0] + " ");
+                            b++;
+                        }
+                        b--;    //last word
+                    }
+                    else dashDone = false;  //else a dash has been processed
+
+
+                    //now check if the line just processed ends with a - (for seperated words) or : (for lists)
+                    if (strcur.Length > 1 && b < proper.Count - 1 && proper[b][0].Length > 0 && (Int32.Parse(proper[b + 1][6]) == -1 || strcur.ToString()[strcur.ToString().Length - 2] == '-'))    //here
+                    {
+                        if (proper[b][0].ElementAt(proper[b][0].Length - 1) == '-' || strcur.ToString()[strcur.ToString().Length - 2] == '-') //if a dash
+                        {
+                            dashDetect = true;
+                            strcur.Remove(strcur.Length - 2, 2);   //remove "- " so word can be continued
+                        }
+
+                        else if (proper[b][0].ElementAt(proper[b][0].Length - 1) == ':' && !listDetect)    //if : initiate a list
+                        {
+                            listDetect = true;
+                            flush = true;
+                            listMarg = LineLeft(proper, b);
+                            margLineNum = b;
+                        }
+                    }
+
+                    if (listDetect)
+                    {
+                        if (strcur.ToString().Contains(';'))    //if in a list and the element contains a semi-colon
+                        {
+                            flush = true;
+                        }
+                        if (strcur.ToString().LastIndexOf('.') == strcur.ToString().Length - 2)    //if in a list and the element contains a semi-colon
+                        {//not getting to here
+                            flush = true;
+                            listDetect = false;
+                        }
+                        //if margin returns to original level in next two lines
+                        //if (CheckLeft(proper, NextLine(proper, b), margLineNum, limit)) //CheckLeft(proper, b, margLineNum, limit) ||
+                        if (CheckLeft(proper, NextLine(proper, b), margLineNum, limit))   //AND HERE
+                        {
+                            listDetect = false;
+                            flush = true;
+                        }
+                    }
+
+
+                    if ((flush && !dashDetect))    //if flushing // || (flush && listDetect)
+                    {
+                        paragraphs.Add(strcur.ToString());
+                        strcur = new StringBuilder();
+                        flush = false;
+                    }
+                    else if (newParNextLine)
+                    {
+                        newParNextLine = false;
+                        flush = true;
+                    }
+
+                    if (strcur.ToString().Contains("same becomes better understood"))
+                    {
+                        //just a general search for testing purposes
+                    }
+                }
+            }
+
+            return paragraphs;
         }
 
     }
 }
+
+
+/*
+public int CaseChecker(List<string[]> proper, int b)    //title, subtitle, word, unknown
+{
+    return 1;
+}
+*/
