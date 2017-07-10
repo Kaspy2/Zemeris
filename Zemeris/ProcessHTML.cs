c947f5c02d7be4c729c55ff0e10088f1a2324eef
ASCENT\nikolai <nikolai.debono.ascent.software>
2017-07-10 12:25:35 +0200

    Commit

---
+++ Zemeris/ProcessHTML.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Zemeris
+{
+    class ProcessHTML
+    {
+        private string fileUrl;// { get; set; }
+        public HtmlDocument htmlDoc { get; set; }
+
+        public ProcessHTML(string fileUrl)
+        {
+            this.fileUrl = fileUrl;
+            htmlDoc = new HtmlDocument();
+            htmlDoc.Load(fileUrl);
+
+        }
+
+        public string getTitle()
+        {
+            return htmlDoc.DocumentNode.SelectSingleNode("//title").InnerText;
+        }
+
+        public List<string> getParagraphs()
+        {
+            var data = htmlDoc.DocumentNode.SelectNodes("//p");
+
+            Regex rgx = new Regex("&nbsp;");
+
+            List<string> paragraphs = new List<string>();
+
+            foreach(var d in data)
+            {
+                string result = rgx.Replace(d.InnerText, " ");
+                paragraphs.Add(result);
+                
+            }
+
+            return paragraphs;
+        }
+
+        public List<string> getHeadings(string h)
+        {
+            var data = htmlDoc.DocumentNode.SelectNodes("//h"+h);
+            Regex rgx = new Regex("&nbsp;");
+
+            List<string> heads = new List<string>();
+
+            foreach (var d in data)
+            {
+                string result = rgx.Replace(d.InnerText, " ");
+                heads.Add(result);
+              
+            }
+
+            return heads;
+        }
+        static void Main(string[] args)
+        {
+            ProcessHTML ph = new ProcessHTML((@"C:/Users/nikolai/Desktop/TestAbby/1page.htm"));
+            Console.WriteLine(ph.getTitle());
+            Console.WriteLine();
+            ph.getParagraphs();
+            Console.ReadLine();
+
+        }
+    }
+}
